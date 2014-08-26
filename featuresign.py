60d95b5f0adb07dfe7146894e65122a37e386f1b
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2014-08-26 13:25:20 +0200

    do more in theano

--- featuresign.py
+++ featuresign.py
@@ -20,50 +20,67 @@ def show_function(f):
     theano.printing.pydotprint(f, ".show_function.png")
     os.system("feh optimal_nz.png")
 
+class Thing(object):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 def generate_functions(A, y, gamma):
-    tA = T.matrix('A')
-    ty = T.vector('y')
-    tx = T.vector('x')
-    ttheta = T.vector('theta')
-    
-    tx0 = T.vector('x0')
-    tx1 = T.vector('x1')
-    tbetas = T.vector('betas')
-    
-    error = lambda x: ((T.dot(tA, x) - ty)**2).sum()
+    full = Thing(A=theano.shared(A),
+                 x=T.vector('x'),
+                 theta=T.vector('theta'))
+
+    # tpart should be a vector of integer indices (e.g. active set indices)
+    tpart = T.lvector('part')
+
+    partial = Thing(A=full.A[:, tpart],
+                    x=full.x[tpart],
+                    theta=full.theta[tpart])
+
+    error = lambda x: ((T.dot(partial.A, x) - y)**2).sum()
     derror = lambda x: T.grad(error(x), x)
     penalty = lambda x: gamma*abs(x).sum()
     loss = lambda x: error(x) + penalty(x)
-    loss_if_consistent = lambda x: error(x) + gamma*T.dot(ttheta, x)
+    dloss = lambda x: T.grad(loss(x), x)
+    loss_if_consistent = lambda x: error(x) + gamma*T.dot(partial.theta, x)
 
-    entering_index = T.argmax(abs(derror(tx)))
-    txs, _ = theano.map(lambda b, x0, x1: (1-b)*x0 + b*x1,
-                        [tbetas], [tx0, tx1])
+    def generate_select_entering():
+        entering_index = T.argmax(abs(derror(partial.x)))
+        return theano.function([tpart, full.x],
+                               [tpart[entering_index],
+                                derror(partial.x)[entering_index]])
 
-    errors = theano.map(error, [txs])[0]
-    losses = theano.map(loss, [txs])[0]
+    def generate_optimize_basis():
+        # original solution
+        tx0 = partial.x
+        # optimized solution
+        tx1 = T.dot(tl.matrix_inverse(T.dot(partial.A.T, partial.A)),
+                    T.dot(partial.A.T, y) - gamma/2*partial.theta)
+
+        # investigate zero crossings between tx0 and tx1
+        tbetas = tx0 / (tx0 - tx1)
+        # investigate tx1
+        tbetas = T.concatenate([tbetas, [1.0]])
+        # only between tx0 and inclusively tx1
+        tbetas = tbetas[(T.lt(0, tbetas) * T.le(tbetas, 1)).nonzero()]
+
+        txbs, _ = theano.map(lambda b: (1-b)*tx0 + b*tx1, [tbetas])
+        tlosses, _ = theano.map(loss, [txbs])
+        # select the optimum
+        txb = txbs[T.argmin(tlosses)]
+
+        return theano.function([tpart, full.x, full.theta],
+                               [T.set_subtensor(partial.x,     txb),
+                                T.set_subtensor(partial.theta, T.sgn(txb))])
 
     return {
-        "loss": theano.function([tA, tx], loss(tx),
-                                givens = {ty: y}),
-        "loss_if_consistent": theano.function([tA, tx, ttheta],
-                                              loss_if_consistent(tx),
-                                              givens = {ty: y}),
-        "select_entering": theano.function([tA, tx],
-                                           [entering_index, derror(tx)[entering_index]],
-                                           givens = {ty: y}),
-        "qp_optimum": theano.function([tA, ttheta],
-                                      T.dot(tl.matrix_inverse(T.dot(tA.T, tA)), T.dot(tA.T, ty) - gamma/2*ttheta),
-                                      givens = {ty: y}),
-        "qp_candidates": theano.function([tA, tbetas, tx0, tx1],
-                                         [txs, losses],
-                                         givens = {ty: y}),
-        "select_candidate": theano.function([tA, tbetas, tx0, tx1],
-                                            txs[T.argmin(losses)],
-                                            givens = {ty: y}),
-        "optimal_nz": theano.function([tA, tx],
-                                      derror(tx) + gamma*T.sgn(tx),
-                                      givens = {ty: y}),
+        "loss": theano.function([tpart, full.x],
+                                loss(partial.x)),
+        "loss_if_consistent": theano.function([tpart, full.x, full.theta],
+                                              loss_if_consistent(partial.x)),
+        "select_entering": generate_select_entering(),
+        "optimize_basis": generate_optimize_basis(),
+        "optimal_nz": theano.function([tpart, full.x], dloss(partial.x)),
         }
 
 # TODO use sparse representations where appropriate
@@ -84,10 +101,8 @@ def l1ls_featuresign(A, y, gamma, x=None):
         zero_mask = x == 0
         if np.any(zero_mask):
             # select entering variable
-            xz = x[zero_mask]
-            iz, l = fs["select_entering"](A[:, zero_mask], xz)
-            # iz is an index into xz; figure out the corresponding index into x
-            i = np.where(zero_mask)[0][iz]
+            part = np.nonzero(zero_mask)[0]
+            i, l = fs["select_entering"](part, x)
 
             if abs(l) > gamma:
                 theta[i] = -np.sign(l)
@@ -100,6 +115,9 @@ def l1ls_featuresign(A, y, gamma, x=None):
             elif not np.any(active):
                 logging.debug("empty basis and no entering variable")
                 break
+            else:
+                logging.debug("no entering variable")
+                break
         elif basis_optimal:
             logging.debug("optimal")
             break
@@ -108,16 +126,16 @@ def l1ls_featuresign(A, y, gamma, x=None):
 
         while not basis_optimal:
             # optimize active variables
-            xnew, thetanew = optimize_basis(A[:, active], x[active], theta[active], fs)
+            part = np.nonzero(active)[0]
+            x, theta = fs["optimize_basis"](part, x, theta)
 
-            x[active] = xnew
-            theta[active] = thetanew
-            active[active] = np.logical_not(np.isclose(xnew, 0))
+            active[active] = np.logical_not(np.isclose(x[active], 0))
+            part = np.nonzero(active)[0]
 
-            logging.debug("x %s theta %s active %s" % (x, theta, active))
+            logging.debug("x %s theta %s" % (x[active], theta[active]))
 
             # check optimality
-            optimal_nz = fs["optimal_nz"](A[:, active], x[active])
+            optimal_nz = fs["optimal_nz"](part, x)
             logging.debug("optimal_nz %s" % optimal_nz)
             if np.allclose(optimal_nz, 0):
                 # maybe let another variable enter
@@ -125,25 +143,3 @@ def l1ls_featuresign(A, y, gamma, x=None):
 
     logging.debug("final x %s" % x)
     return x
-
-def optimize_basis(A, x0, theta, fs):
-    x1 = fs["qp_optimum"](A, theta)
-    lossc0, lossc1 = map(lambda x: fs["loss_if_consistent"](A, x, theta), [x0, x1])
-    loss0, loss1 = map(lambda x: fs["loss"](A, x), [x0, x1])
-    logging.debug("qp_optimum x0 %s lossc0 %f loss0 %f" % (x0, lossc0, loss0))
-    logging.debug("qp_optimum x1 %s lossc1 %f loss1 %f" % (x1, lossc1, loss1))
-
-    # find zero-crossings
-    betas = x0 / (x0 - x1)
-    betas = betas[np.logical_and(0 < betas, betas < 1)]
-    # make sure we investigate x1
-    betas = np.append(betas, 1)
-
-    xs, losses = fs["qp_candidates"](A, betas, x0, x1)
-    logging.debug("betas %s xs %s losses %s" % (betas, xs, losses))
-
-    x = xs[np.argmin(losses)]
-    logging.debug("selected candidate %s" % x)
-
-    theta = np.sign(x)
-    return x, theta
